154b29d832bc67ff0357a598e217293ca03c1816
mirjanTra <mirjana.trajanovski@gmail.com>
2026-01-20 11:55:13 +0100

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   Ime: <?php echo $_POST["first"]; ?> <br>
+   Prezime: <?php echo $_POST["last"]; ?> <br>
+   Email: <?php echo $_POST["email"]; ?> <br>
+   Telefon: <?php echo $_POST["phone"]; ?><br>
+   Poruka: <?php echo $_POST["message"]; ?>
+
+
+</body>
+</html>
